3d410c4e559ef6088653246d7f0171e22383a0ea
João Victor Araujo <118845832+JoaoVictor-A-F-R@users.noreply.github.com>
2025-02-04 01:45:46 +0000

    Nivel mestre completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,14 +6,14 @@
 int rep = 1;
 int cd_cidade[2];
 char nm_cidade[2][50];
-int populacao[2], num_pt_tur[2], area[2];
+int populacao[2], num_pt_tur[2], area[2], densidade_pop[2], trunfo[2];
 float pib[2];
-double densidade_pop[2], pib_capita[2];
+double pib_capita[2];
 
 int Entrada_dados(int i){
     //Recebe os dados do usuário
     int comparar;
-    printf("========== Carta %d ==========\n", i+1 );
+    printf("============== Carta %d ==============\n", i+1 );
     printf("Digite o nome da Cidade: ");
     fgets(nm_cidade[i], sizeof(nm_cidade[i]), stdin);
 
@@ -40,11 +40,16 @@ int Entrada_dados(int i){
         rep = !rep;
         Entrada_dados(i+1);
     }
-    
-    printf("==============================\n");
-    printf("Qual atributo comparar?");
-    printf("1 - Populacao\n2- Numero de Pontos Turisticos\n3 - Area\n4 - PIB\n5 - Densidade Populacional\n6 - PIB per Capita");
-    scanf("%d", &comparar);
+    else
+    {
+        printf("==============================\n");
+        printf("1 - Populacao\n2- Numero de Pontos Turisticos\n3 - Area\n4 - PIB\n5 - Densidade Populacional\n6 - PIB per Capita\n7 - Super Poder\n");
+        printf("Qual atributo comparar? ");
+        scanf("%d", &comparar);
+        getchar();
+
+        Exibir_carta(0, comparar);
+    }
 
     return 0;
 }
@@ -52,22 +57,128 @@ int Entrada_dados(int i){
 int Processamento_dados(int i){
     //Realiza o calculo dos dados necesarios para exibição das cartas
     cd_cidade[i] += 1;
-    densidade_pop[i] = (double) populacao[i] / area[i];
+    densidade_pop[i] = populacao[i] / area[i];
     pib_capita[i] = (double) pib[i] / populacao[i];
+    trunfo[i] = (int) populacao[i] + area[i] + pib[i] + pib_capita[i] + densidade_pop[i] + num_pt_tur[i];
     return 0;
 }
 
-int Exibir_carta(int i) {
-    printf("======================================================\n");
-    printf("Código da Cidade: #0%d #0%d\n", cd_cidade[i], cd_cidade[i+1]);
+int Comparacao(int comparar){
+    int i = 0;
+    return 0;
+}
+
+int Exibir_carta(int i, int comparar) {
+    //int tamanho = strlen(nm_cidade[i]);
+    //nm_cidade[i][tamanho - 1] = "";
+    printf("===============================================================================\n");
+    printf("Código da Cidade: #0%d || #0%d\n", cd_cidade[i], cd_cidade[i+1]);
     printf("Nome da Cidade: %s || %s", nm_cidade[i], nm_cidade[i+1]);
-    printf("População: %d\n", populacao[i]);
-    printf("Numero de pontos Turísticos: %d\n", num_pt_tur[i]);
-    printf("Área da cidade: %d KM\n", area[i]);
-    printf("PIB: %.2f R$\n", pib[i]);
-    printf("Densidade Populacional: %.2lf HAB/KM²\n", densidade_pop[i]);
-    printf("PIB per Capita: %.2lf R$\n", pib_capita[i]);
-    printf("======================================================\n");
+    switch (comparar)
+    {
+    case 1:
+        if (populacao[i] > populacao[i + 1]){
+            printf("População: %d (+) || %d\n", populacao[i], populacao[i+1]);
+        }
+        else{
+            printf("População: %d || %d (+)\n", populacao[i], populacao[i+1]);
+        }
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+    case 2:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        if (num_pt_tur[i] > num_pt_tur[i + 1]){
+            printf("Numero de pontos Turísticos: %d (+) || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        }else{
+            printf("Numero de pontos Turísticos: %d || %d (+)\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        }
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+    
+    case 3:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        if (area[i] > area[i + 1])
+        {
+            printf("Área da cidade: %d KM (+) || %d KM\n", area[i], area[i + 1]);
+        }else{
+            printf("Área da cidade: %d KM || %d KM (+) \n", area[i], area[i + 1]);
+        }
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+
+    case 4:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        if (pib[i] > pib[i + 1])
+        {
+            printf("PIB: %.2f R$ (+) || %.2f R$\n", pib[i], pib[i + 1]);
+        }else{
+            printf("PIB: %.2f R$ || %.2f R$ (+)\n", pib[i], pib[i + 1]);
+        }
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+    case 5:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        if (densidade_pop[i] > densidade_pop[i + 1])
+        {
+            printf("Densidade Populacional: %.d HAB/KM² (+) || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        }else{
+            printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM² (+)\n", densidade_pop[i], densidade_pop[i + 1]);
+        }
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+    
+    case 6:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        if (pib_capita[i] > pib_capita[i + 1])
+        {
+            printf("PIB per Capita: %.2lf R$ (+) || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        }else{
+            printf("PIB per Capita: %.2lf R$ || %.2lf R$ (+)\n", pib_capita[i], pib_capita[i + 1]);
+        }
+        printf("Super Poder: %d || %d \n", trunfo[i], trunfo[i + 1]);
+        break;
+    
+    case 7:
+        printf("População: %d || %d\n",populacao[i], populacao[i + 1]);
+        printf("Numero de pontos Turísticos: %d || %d\n", num_pt_tur[i], num_pt_tur[i + 1]);
+        printf("Área da cidade: %d KM || %d KM\n", area[i], area[i + 1]);
+        printf("PIB: %.2f R$ || %.2f R$\n", pib[i], pib[i + 1]);
+        printf("Densidade Populacional: %.d HAB/KM² || %.d HAB/KM²\n", densidade_pop[i], densidade_pop[i + 1]);
+        printf("PIB per Capita: %.2lf R$ || %.2lf R$\n", pib_capita[i], pib_capita[i + 1]);
+        if(trunfo[i] > trunfo[i + 1])
+        {
+            printf("Super Poder: %d (+) || %d\n", trunfo[i], trunfo[i + 1]);
+        }else{
+            printf("Super Poder: %d || %d (+)\n", trunfo[i], trunfo[i + 1]);
+        }
+        break;
+    }
+    printf("===============================================================================\n");
 
     return 0;
 }
@@ -76,6 +187,6 @@ int main() {
     //carta c;
     int i = 0;
     Entrada_dados(i);
-    Exibir_carta(i);
+    //Exibir_carta(i);
     return 0;
 }
